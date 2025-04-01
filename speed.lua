998583481c643bb0da875aef9dd3c3fb7322c2fe
VertiGhost222 <lilianguillemot@gmail.com>
2025-04-01 19:20:31 +0200

    Update speed.lua

--- speed.lua
+++ speed.lua
@@ -6,15 +6,6 @@ local SpeedTab = Window:NewTab("Vitesse")
 local SpeedSection = SpeedTab:NewSection("Ajuster")
 local walkspeed = 16
 SpeedSection:NewLabel("Créé par VertiGhost")
-SpeedSection:NewTextBox("Vitesse", "Entre une valeur", function(value)
-    walkspeed = tonumber(value)
-end)
-SpeedSection:NewButton("Appliquer", "Change la vitesse", function()
-    if walkspeed then
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = walkspeed
-    end
-end)
-SpeedSection:NewButton("Réinitialiser", "Vitesse à 16", function()
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-    walkspeed = 16
-end)
+SpeedSection:NewTextBox("Vitesse", "Entre une valeur", function(value) walkspeed = tonumber(value) end)
+SpeedSection:NewButton("Appliquer", "Change la vitesse", function() if walkspeed then game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = walkspeed end end)
+SpeedSection:NewButton("Réinitialiser", "Vitesse à 16", function() game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16 walkspeed = 16 end)
