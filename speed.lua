bece69c30e950c815f75e31d0b8df8eeb9054e82
VertiGhost222 <lilianguillemot@gmail.com>
2025-04-01 19:26:31 +0200

    Update speed.lua

--- speed.lua
+++ speed.lua
@@ -2,10 +2,19 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Librar
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Brookhaven Speed", "Sentinel")
-local SpeedTab = Window:NewTab("Vitesse")
-local SpeedSection = SpeedTab:NewSection("Ajuster")
+local SpeedTab = Window:NewTab("Speed")
+local SpeedSection = SpeedTab:NewSection("Adjust")
 local walkspeed = 16
-SpeedSection:NewLabel("Créé par VertiGhost")
-SpeedSection:NewTextBox("Vitesse", "Entre une valeur", function(value) walkspeed = tonumber(value) end)
-SpeedSection:NewButton("Appliquer", "Change la vitesse", function() if walkspeed then game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = walkspeed end end)
-SpeedSection:NewButton("Réinitialiser", "Vitesse à 16", function() game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16 walkspeed = 16 end)
+SpeedSection:NewLabel("Created by VertiGhost")
+SpeedSection:NewTextBox("Speed", "Enter a value", function(value)
+    walkspeed = tonumber(value)
+end)
+SpeedSection:NewButton("Apply", "Change the speed", function()
+    if walkspeed then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = walkspeed
+    end
+end)
+SpeedSection:NewButton("Reset", "Speed to 16", function()
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+    walkspeed = 16
+end)
